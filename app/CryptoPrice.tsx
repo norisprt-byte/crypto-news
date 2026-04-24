17a1058dbab8b25e4db0dae9c171eef6286dfa1f
norisprt <noris.prt@gmail.com>
2026-04-25 05:06:53 +0700

    update project

--- app/CryptoPrice.tsx
+++ app/CryptoPrice.tsx
@@ -114,7 +114,7 @@ export default function CryptoPrice() {
           textAlign: "center",
         }}
       >
-        📊 REAL MARKET TERMINAL (v4)
+       
       </div>
 
       {/* GRID CENTERED */}
